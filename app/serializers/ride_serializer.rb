65147b0f2d27897ba6d97d0860f930ed2ca36abf
Daniel Chen <daniel.chen115@gmail.com>
2018-11-07 18:47:30 -0500

    Serialize rides with bookings

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,3 +1,4 @@
 class RideSerializer < ActiveModel::Serializer
-  attributes :id, :origin, :destination, :leave_at, :arrive_at, :price, :seats
+  attributes :id, :origin, :destination, :leave_at, :arrive_at, :price, :seats, :details, :created_at, :updated_at
+  has_many :bookings
 end
